b36ece6afb301a2828df331b7cab7b902c7bb1ea
Ziyang tan <ziyang0621@gmail.com>
2016-02-08 17:20:38 -0800

    updated notetaker project

---
+++ githubNotetaker/App/Components/Badge.js
@@ -0,0 +1,51 @@
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+     backgroundColor: '#48BBEC',
+     paddingBottom: 10
+   },
+   name: {
+     alignSelf: 'center',
+     fontSize: 21,
+     marginTop: 10,
+     marginBottom: 5,
+     color: 'white'
+   },
+   handle: {
+     alignSelf: 'center',
+     fontSize: 16,
+     color: 'white'
+   },
+   image: {
+     height: 125,
+     width: 125,
+     borderRadius: 65,
+     marginTop: 10,
+     alignSelf: 'center'
+   }
+});
+
+class Badge extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Image style={styles.iamge} source={{uri: this.props.userInfo.avatar_url}} />
+          <Text style={styles.name}> {this.props.userInfo.name} </Text>
+          <Text style={styles.handle}> {this.props.userInfo.login} </Text>
+        </View>
+    )
+  };
+}
+
+Badge.propTypes = {
+  userInfo: PropTypes.object.isRequired
+}
+
+module.exports = Badge;
